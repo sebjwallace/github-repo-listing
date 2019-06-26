ca860522a5286360e9639790418d8953d1a2d350
sebjwallace <sebjwallace@gmail.com>
2019-06-26 21:39:48 +0100

    add api test for 400 response

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -6,38 +6,59 @@ const app = require('../index');
 const { github } = require('../config');
 
 const mocks = {
-    users: {
-        response: {
-            asc: require('./mocks/users.asc.response')
-        }
-    }
+	users: {
+		response: {
+			asc: require('./mocks/users.asc.response')
+		}
+	}
 }
 
 describe('GET user repos', () => {
 
-  it('should return a user\'s repos', done => {
+	describe('errors', () => {
 
-    const query = {
-        direction: 'asc',
-        sort: 'created',
-        type: 'all'
-    };
+		it('should throw 400 with invalid query params', done => {
+	
+			request(app)
+					.get('/users/sebjwallace')
+					.query({})
+					.end((err, res) => {
+							expect(err).to.eq(null);
+							expect(res.status).to.eq(400);
+							done();
+					});
+	
+		});
 
-    nock(github.url)
-        .get('/users/sebjwallace/repos')
-        .query(query)
-        .reply(200, mocks.users.response.asc);
+	})
 
-    request(app)
-        .get('/users/sebjwallace')
-        .query(query)
-        .end((err, res) => {
-            expect(err).to.eq(null);
-            expect(res.status).to.eq(200)
-            expect(res.body).to.deep.eq(mocks.users.response.asc);
-            done();
-        })
+	describe('success', () => {
 
-  });
+		it('should return a user\'s repos', done => {
+
+			const query = {
+				direction: 'asc',
+				sort: 'created',
+				type: 'all'
+			};
+	
+			nock(github.url)
+				.get('/users/sebjwallace/repos')
+				.query(query)
+				.reply(200, mocks.users.response.asc);
+	
+			request(app)
+				.get('/users/sebjwallace')
+				.query(query)
+				.end((err, res) => {
+					expect(err).to.eq(null);
+					expect(res.status).to.eq(200);
+					expect(res.body).to.deep.eq(mocks.users.response.asc);
+					done();
+				});
+	
+		});
+
+	})
 
 });
